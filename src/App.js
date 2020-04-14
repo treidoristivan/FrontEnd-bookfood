f7d826186b8f8ff67e1c22a351824843115e5379
treidoristivan <ristivantreido@gmail.com>
2020-04-15 01:25:55 +0700

    Router Name

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import Page403 from './views/Page403'
 
 function App (props) {
   return (
-    <Router>
+    <Router basename={(process.env.PUBLIC_URL || '/')}>
       <Switch>
         <GuestRoute
           exact
